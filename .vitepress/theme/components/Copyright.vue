875f2ca1164dc1f9695e9b75d765931c79709778
airene <linfanne@gmail.com>
2022-10-31 20:44:54 +0800

    bump vitepress version 1.0.0-alpha.26 & modify copyright

--- .vitepress/theme/components/Copyright.vue
+++ .vitepress/theme/components/Copyright.vue
@@ -1,9 +1,8 @@
 <template>
     <div class="site-footer">
-        MIT Licensed | Copyright © 2021-2022 <a class="vitepress" :href="website">{{ webTitle }}</a
-        ><br />
-        Powered by
-        <a class="vitepress" target="_blank" href="//vitepress.vuejs.org/">VitePress - 1.0.0-alpha.15</a>
+        MIT Licensed | Copyright © 2021-2022 <a class="vitepress" :href="website">{{ webTitle }}</a><br />
+        Powered by <a class="vitepress" target="_blank" href="//vitepress.vuejs.org/">VitePress - 1.0.0-alpha.26</a>
+        Theme by <a class="vitepress" target="_blank" href="//github.com/airene/vitepress-blog-pure">Vitepress-blog-pure</a>
     </div>
 </template>
 <script lang="ts" setup>
