731ffffece2a5076b41d8f93bf427b2e73ea5131
fangying <ying.fang@livepremiere.ai>
2026-01-18 21:17:37 +0800

    bump version

--- .vitepress/theme/components/Copyright.vue
+++ .vitepress/theme/components/Copyright.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="site-footer">
-        MIT Licensed | Copyright © 2021-2022 <a class="vitepress" :href="website">{{ webTitle }}</a><br />
+        MIT Licensed | Copyright © 2021-present <a class="vitepress" :href="website">{{ webTitle }}</a><br />
         Powered by <a class="vitepress" target="_blank" href="//vitepress.vuejs.org/">VitePress - 1.6.4</a>
         Theme by <a class="vitepress" target="_blank" href="//github.com/airene/vitepress-blog-pure">Vitepress-blog</a>
     </div>
